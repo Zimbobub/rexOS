3b5e937c6a6aa0e52a0987fe46f18ba28fff0833
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2023-09-09 15:34:39 +1000

    refactored stage1 boot

--- src/boot/stage1/boot.asm
+++ src/boot/stage1/boot.asm
@@ -8,73 +8,35 @@ mov al, 0x3
 int 0x10                ; text mode (clears screen)
 
 
-mov [BOOT_DISK], dl
+read_stage2:
+    mov [BOOT_DISK], dl
 
 
-xor ax, ax
-mov es, ax
-mov ds, ax
-mov bp, 0x8000
-mov sp, bp
+    xor ax, ax
+    mov es, ax
+    mov ds, ax
+    mov bp, 0x8000
+    mov sp, bp
 
-mov bx, STAGE2_LOCATION
-mov dh, 2
+    mov bx, STAGE2_LOCATION
+    mov dh, 2
 
-mov ah, 0x02
-mov al, dh
-mov ch, 0x00    
-mov dh, 0x00
-mov cl, 0x02
-mov dl, [BOOT_DISK]
-int 0x13                ; no error management
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00    
+    mov dh, 0x00
+    mov cl, 0x02
+    mov dl, [BOOT_DISK]
+    int 0x13                ; no error management yet
 
 
-; start moving toward 32 bits
-CODE_SEG equ GDT_code - GDT_start
-DATA_SEG equ GDT_data - GDT_start
-
-cli
-lgdt [GDT_descriptor]
-mov eax, cr0
-or eax, 1
-mov cr0, eax
-jmp CODE_SEG:start_protected_mode
-
-; hang if we somehow get here
-jmp $
-                                    
-BOOT_DISK: db 0
-
-GDT_start:
-    GDT_null:
-        dd 0x0
-        dd 0x0
-
-    GDT_code:
-        dw 0xffff
-        dw 0x0
-        db 0x0
-        db 0b10011010
-        db 0b11001111
-        db 0x0
-
-    GDT_data:
-        dw 0xffff
-        dw 0x0
-        db 0x0
-        db 0b10010010
-        db 0b11001111
-        db 0x0
-
-GDT_end:
-
-GDT_descriptor:
-    dw GDT_end - GDT_start - 1
-    dd GDT_start
+; enter protected mode
+%include "src/boot/stage1/enter_protected.asm"
 
 
 [bits 32]
 start_protected_mode:
+    ; just set all segments to be the same
     mov ax, DATA_SEG
 	mov ds, ax
 	mov ss, ax
